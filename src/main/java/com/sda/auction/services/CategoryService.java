b6e18f5138d807475a792d9750aade4457b13d32
Spinu Andrei <aandreispinu@gmail.com>
2020-12-20 11:47:32 +0200

    Added CategoryService/CategoryServiceImpl

--- src/main/java/com/sda/auction/services/CategoryService.java
+++ src/main/java/com/sda/auction/services/CategoryService.java
@@ -1,4 +1,17 @@
 package com.sda.auction.services;
 
+import com.sda.auction.entitites.Category;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public interface CategoryService {
+    void createCategories(Category category);
+    List<Category> findAllCategories();
+    Optional<Category> findById(int id);
+    void updateCategory(Category category);
+    void deleteById(int id);
+
 }
--- src/main/java/com/sda/auction/services/CategoryServiceImpl.java
+++ src/main/java/com/sda/auction/services/CategoryServiceImpl.java
@@ -1,12 +1,49 @@
 package com.sda.auction.services;
 
+import com.sda.auction.entitites.Category;
+import com.sda.auction.repositories.CategoryRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 @AllArgsConstructor(onConstructor = @__(@Autowired))
-public class CategoryServiceImpl implements CategoryService {
-    private final CategoryService categoryService;
+public class CategoryServiceImpl implements CategoryService{
 
+    private final CategoryRepository categoryRepository;
+
+    @Override
+    public void createCategories(Category category) {
+        categoryRepository.save(category);
+    }
+
+    @Override
+    public List<Category> findAllCategories() {
+        return categoryRepository.findAll();
+    }
+
+    @Override
+    public Optional<Category> findById(int id) {
+        return categoryRepository.findById(id);
+    }
+
+    @Override
+    public void updateCategory(Category category) {
+        Optional<Category> categoryOptional = categoryRepository.findById(category.getId());
+        if(categoryOptional.isPresent()){
+            categoryOptional.get().setDescription(category.getDescription());
+            categoryOptional.get().setName(category.getName());
+            categoryOptional.get().setAuctionList(category.getAuctionList());
+            categoryRepository.save(categoryOptional.get());
+        }
+
+    }
+
+    @Override
+    public void deleteById(int id) {
+        categoryRepository.deleteById(id);
+    }
 }
