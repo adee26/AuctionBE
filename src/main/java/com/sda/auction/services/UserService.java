49c952381231daad6e9348345af0b6d7bc357e59
Adelina <apetreiadelina@yahoo.com>
2020-12-20 10:27:00 +0200

    Added user CRUD operations.

--- src/main/java/com/sda/auction/services/UserService.java
+++ src/main/java/com/sda/auction/services/UserService.java
@@ -1,4 +1,14 @@
 package com.sda.auction.services;
 
+import com.sda.auction.entitites.User;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface UserService {
+    void createUser(User user);
+    List<User> findAllUsers();
+    Optional<User> findById(int id);
+    void deleteById(int id);
+    void updateUser(User user);
 }
--- src/main/java/com/sda/auction/services/UserServiceImpl.java
+++ src/main/java/com/sda/auction/services/UserServiceImpl.java
@@ -1,12 +1,49 @@
 package com.sda.auction.services;
 
+import com.sda.auction.entitites.User;
 import com.sda.auction.repositories.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 @AllArgsConstructor(onConstructor = @__(@Autowired))
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
+
+    @Override
+    public void createUser(User user) {
+        userRepository.save(user);
+    }
+
+    @Override
+    public List<User> findAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public Optional<User> findById(int id) {
+        return userRepository.findById(id);
+    }
+
+    @Override
+    public void deleteById(int id) {
+        userRepository.deleteById(id);
+    }
+
+    @Override
+    public void updateUser(User user) {
+        Optional<User> userOptional = userRepository.findById(user.getId());
+        if(userOptional.isPresent()){
+            userOptional.get().setAddress(user.getAddress());
+            userOptional.get().setAccountType(user.getAccountType());
+            userOptional.get().setEmail(user.getEmail());
+            userOptional.get().setName(user.getName());
+            userOptional.get().setPassword(user.getPassword());
+            userRepository.save(userOptional.get());
+        }
+    }
 }
