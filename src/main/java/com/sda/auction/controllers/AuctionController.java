f37adc300e8ab1a4501cc6d1633bdab8ec8983e8
Spinu Andrei <aandreispinu@gmail.com>
2021-01-07 13:56:01 +0200

    added recentlyAdded, recentlyEnded and endding auctions GET methods

--- src/main/java/com/sda/auction/controllers/AuctionController.java
+++ src/main/java/com/sda/auction/controllers/AuctionController.java
@@ -5,8 +5,14 @@ import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @RestController
 @RequestMapping("/api/v1/auction")
@@ -39,4 +45,32 @@ public class AuctionController {
         auctionService.deleteById(id);
     }
 
+    @GetMapping("/recently")
+    public List<Auction> findFourRecentlyAddedAuctions(){
+        return auctionService.findAllAuctions()
+                .stream()
+                .sorted(Comparator.comparing(Auction::getStartDate).reversed())
+                .limit(4)
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/ending")
+    public List<Auction> findFourEndingAuctions(){
+        return auctionService.findAllAuctions()
+                .stream()
+                .sorted(Comparator.comparing(Auction::getEndDate))
+                .filter(auction -> auction.getEndDate().isAfter(LocalDateTime.ofInstant(Instant.now(),  ZoneId.of("Europe/Bucharest"))))
+                .limit(4)
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/recentlyEnded")
+    public List<Auction> findFourRecentlyEndedAuctions(){
+        return this.auctionService.findAllAuctions()
+                .stream()
+                .sorted(Comparator.comparing(Auction::getEndDate).reversed())
+                .filter(auction -> auction.getEndDate().isBefore(LocalDateTime.ofInstant(Instant.now(),  ZoneId.of("Europe/Bucharest"))))
+                .limit(4)
+                .collect(Collectors.toList());
+    }
 }
