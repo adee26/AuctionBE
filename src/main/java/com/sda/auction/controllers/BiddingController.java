a2d42c3b92410d6d82e71ec79f7ec580ccdc3e28
Adelina <apetreiadelina@yahoo.com>
2021-02-21 11:46:38 +0200

    Changed bidding controller link.

--- src/main/java/com/sda/auction/controllers/BiddingController.java
+++ src/main/java/com/sda/auction/controllers/BiddingController.java
@@ -10,17 +10,17 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/bidding")
+@RequestMapping()
 @AllArgsConstructor(onConstructor = @__(@Autowired))
 public class BiddingController {
     private final BiddingService biddingService;
 
-    @GetMapping("/{id}")
+    @GetMapping("api/v1/bidding")
     public List<Bidding> findBiddingByUserId(@PathVariable("id") int id){
         return biddingService.findBiddingByUserId(id);
     }
 
-    @PostMapping ("{auctionId}/bid")
+    @PostMapping ("api/v1/auction/{auctionId}")
     public void placeBid(@PathVariable("auctionId") int auctionId, @RequestBody BidDTO bidDTO) throws Exception {
         biddingService.placeBid(auctionId, bidDTO);
     }
